e7b481b037315941cb88c3654603b4d16657abf8
cylcop <1228413017@qq.com>
2022-11-30 22:12:23 +0800

    Update HelloIDEA.java File. Add Two Lines.

--- HelloIDEA.java
+++ HelloIDEA.java
@@ -2,7 +2,8 @@ package com.cops;
 
 public class HelloIDEA {
   public static void main(String[] args) {
-    System.out.println("Hello IDEA!");
+    System.out.println("Hello World!");
+    System.out.println("Hello COPS!");
   }
 
 
