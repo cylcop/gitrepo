04d5f24ce530ac631cb2ec00323b2ebfa3d6bff8
cylcop <1228413017@qq.com>
2022-12-22 16:47:28 +0800

    Change Two Files Print Content

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ package com.cops;
 
 public class Demo{
   public static void main(String[] args) {
-    System.out.println("Hello COPS!");
+    System.out.println("Hello Demo!");
   }
 
 
--- HelloIDEA.java
+++ HelloIDEA.java
@@ -2,7 +2,7 @@ package com.cops;
 
 public class HelloIDEA {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello IDEA!");
   }
 
 
